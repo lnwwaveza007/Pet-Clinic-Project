70a9979de0bfeac2c8b6a3768089c428c49d30ba
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2024-06-07 01:13:23 +0700

    I forget to test code.

--- src/main/java/org/group11/Main.java
+++ src/main/java/org/group11/Main.java
@@ -5,28 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Console console = System.console();
-        while (true) {
-            System.out.println("Hello world!");
-            System.out.println("1. To enter room");
-            System.out.println("2. To exit room");
-            if (console.readLine().equalsIgnoreCase("1")) {
-                room();
-            } else {
-                break;
-            }
-        }
-    }
-
-    public static void room() {
-        Console console = System.console();
-        System.out.println("Yay you're now in room!");
-        System.out.println("1. To enter room");
-        System.out.println("2. To exit room");
-        if (console.readLine().equalsIgnoreCase("1")) {
-            System.out.println("You entered the room");
-        } else {
-            return;
-        }
+        System.out.println("Hello, World!");
     }
 }
