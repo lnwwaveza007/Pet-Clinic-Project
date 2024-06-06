3b8b5a11dbcf69907eb181219ac409d1e96590b6
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2024-06-07 01:10:12 +0700

    getOwner and Checkpassword

--- src/main/java/org/group11/Main.java
+++ src/main/java/org/group11/Main.java
@@ -1,7 +1,32 @@
 package org.group11;
 
+import java.io.Console;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Console console = System.console();
+        while (true) {
+            System.out.println("Hello world!");
+            System.out.println("1. To enter room");
+            System.out.println("2. To exit room");
+            if (console.readLine().equalsIgnoreCase("1")) {
+                room();
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void room() {
+        Console console = System.console();
+        System.out.println("Yay you're now in room!");
+        System.out.println("1. To enter room");
+        System.out.println("2. To exit room");
+        if (console.readLine().equalsIgnoreCase("1")) {
+            System.out.println("You entered the room");
+        } else {
+            return;
+        }
     }
 }
