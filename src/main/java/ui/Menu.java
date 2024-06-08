699dcc0c577ab4d582d6886cea7a457d8ec41983
opetoi <117988644+opetoi@users.noreply.github.com>
2024-06-08 15:23:30 +0700

    Create Admin, Owner,Vet, Medical History View

--- src/main/java/ui/Menu.java
+++ src/main/java/ui/Menu.java
@@ -1,13 +1,19 @@
 package ui;
 
 import java.io.*;
+import repository.*;
 import services.ClinicServices;
 
 
 public class Menu {
-    static Console console = System.console();
+    private final ClinicServices clinicServices;
+    Console console = System.console();
+    public Menu(OwnerRepository ownerRepository, PetRepository petRepository, AppointmentRepository appointmentRepository, MedHisRepository medHisRepository, VetRepository vetRepository) {
+        clinicServices = new ClinicServices(ownerRepository, petRepository, appointmentRepository, medHisRepository, vetRepository);
+    }
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
+        ClinicServices clinicServices = new ClinicServices(null, null, null, null, null);
         while (true) {
             mainMenu();
             System.out.println("Enter your choice: ");
@@ -18,13 +24,13 @@ public class Menu {
                     adminMenu(console);
                     break;
                 case "2":
-                    ownerMenu(console);
+                    loginOwner(console); //login
                     break;
                 case "3":
-                    createOwner();
+                    register(console);
                     break;
                 case "4":
-                    vetMenu(console);
+                    loginVet(console);
                     break;
                 case "5":
                     System.out.println("Exit!");
@@ -36,7 +42,7 @@ public class Menu {
         }
     }
 
-    public static void mainMenu() {
+    public void mainMenu() {
         System.out.println("Welcome to Main Menu");
         System.out.println("1: Admin Menu");
         System.out.println("2: Owner's Menu");
@@ -45,11 +51,58 @@ public class Menu {
         System.out.println("5: Exit");
     }
 
-    public static void adminMenu(Console console) {
+    //register
+    public void register(Console console) {
+        System.out.println("Register Menu");
+        System.out.println("Enter your ID Card: ");
+        String idCard = console.readLine();
+        System.out.println("Enter your Password: ");
+        String password = console.readLine();
+        System.out.println("Enter your Name: ");
+        String name = console.readLine();
+        System.out.println("Enter your Address: ");
+        String address = console.readLine();
+        System.out.println("Enter your Phone Number: ");
+        String phone = console.readLine();
+        clinicServices.createOwner();
+    }
+
+    //login
+    public void loginOwner(Console console) {
+        System.out.println("Enter your ID Card: ");
+        String idCard = console.readLine();
+
+        System.out.println("Enter your Password: ");
+        char[] password = console.readPassword();
+        if (clinicServices.checkPassword()) {
+            ownerMenu(console);
+        }
+    }
+
+    public void loginVet(Console console) {
+        System.out.println("Enter your ID Card: ");
+        String idCard = console.readLine();
+
+        System.out.println("Enter your Password: ");
+        char[] password = console.readPassword();
+        if (clinicServices.checkPassword()) {
+            vetMenu(console);
+        }
+    }
+
+    public void loginAdmin(Console console) {
+        System.out.println("Enter Password: ");
+        String password = console.readLine();
+
+    }
+
+
+    //menu
+    public void adminMenu(Console console) {
         System.out.println("Admin Menu!");
         System.out.println("1: Create Vet");
         System.out.println("2: Remove Vet");
-        System.out.println("3: View all Apointments");
+        System.out.println("3: View all Appointments");
         System.out.println("4: View all Owners");
         System.out.println("5: View all Pets");
         System.out.println("6: Check Vet");
@@ -59,25 +112,25 @@ public class Menu {
         var value = console.readLine();
         switch (value) {
             case "1":
-                createVet();
+                createVetMenu(console);
                 break;
             case "2":
-                removeVet();
+                clinicServices.removeVet();
                 break;
             case "3":
-                getAllAppointments();
+                clinicServices.getAllAppointments();
                 break;
             case "4":
-                //เอา getallowner มาาาาาาาาาาา!!!!!!!!!!!!
+                //clinicServices.getAllOwner();
                 break;
             case "5":
-                getAllPetByOwner();
+                clinicServices.getAllPetByOwner();
                 break;
             case "6":
-                //เอา getallVet มาาาาาาาาาาา!!!!!!!!!!!!
+                //clinicServices.getAllVet();
                 break;
             case "7":
-                medicalHistory(console);
+                adminMedicalHistory(console);
                 break;
             case "8":
                 System.out.println("Exit!");
@@ -88,11 +141,12 @@ public class Menu {
         }
 
     }
-    public static void ownerMenu(Console console) {
+
+    public void ownerMenu(Console console) {
         System.out.println("Owner Menu!");
         System.out.println("1: Show All Pets"); //list pet + enter pet view
         System.out.println("2: Add Pet Data");
-        System.out.println("3: Check All Appointments");
+        System.out.println("3: View All Appointments"); //update & cancel?
         System.out.println("4: Medical History's");
         System.out.println("5: Change Password");
         System.out.println("6: Exit");
@@ -100,16 +154,16 @@ public class Menu {
         var value = console.readLine();
         switch (value) {
             case "1":
-                getAllPetByOwner();
+                clinicServices.getAllPetByOwner();
                 break;
             case "2":
-                addPet();
+                clinicServices.addPet();
                 break;
             case "3":
-                getAllAppointmentByOwner();
+                clinicServices.getAllAppointmentByOwner();
                 break;
             case "4":
-                getAllMedicalHistoryByOwner();
+                clinicServices.getAllMedicalHistoryByOwner();
                 break;
             case "5":
                 System.out.println("Exit!");
@@ -121,27 +175,27 @@ public class Menu {
 
     }
 
-    public static void vetMenu(Console console) {
+    public void vetMenu(Console console) {
         System.out.println("Vet Menu!");
         System.out.println("1: Create Appointment");
         System.out.println("2: Cancel Appointments");
-        System.out.println("3: View Appointments"); // update appointments
+        System.out.println("3: View Appointments"); // update appointments & cancel
         System.out.println("4: Medical History's");
         System.out.println("5: Exit");
 
         var value = console.readLine();
-        switch(value) {
+        switch (value) {
             case "1":
-                createAppointment();
+                clinicServices.createAppointment();
                 break;
             case "2":
-                deleteAppointment();
+                clinicServices.deleteAppointment();
                 break;
             case "3":
-                getAllAppointmentByVet();
+                clinicServices.getAllAppointmentByVet();
                 break;
             case "4":
-                getAllMedicalHistoryByVet();
+                clinicServices.getAllMedicalHistoryByVet();
                 break;
             case "5":
                 System.out.println("Exit!");
@@ -152,7 +206,35 @@ public class Menu {
 
     }
 
-    public static void adminMedicalHistory(Console console) {
+    public void petView(Console console) {
+        System.out.println("Pet Menu    !");
+        System.out.println("1: Check Status");
+        System.out.println("2: View Appointments"); // cancel?
+        System.out.println("3: Create Appointment");
+        System.out.println("4: Cancel Appointments");
+        System.out.println("5: Exit");
+
+        var value = console.readLine();
+        switch (value) {
+            case "1":
+                //checkStatus();
+                break;
+            case "2":
+                clinicServices.getAllAppointmentByPet();
+                break;
+            case "3":
+                clinicServices.createAppointment();
+                break;
+            case "4":
+                clinicServices.deleteAppointment();
+                return;
+            default:
+                System.out.println("Please Enter a Number");
+        }
+    }
+
+    //medical history
+    public void adminMedicalHistory(Console console) {
         System.out.println("Medical History!");
         System.out.println("1: View Medical History From Owner");
         System.out.println("2: View Medical History From Pet");
@@ -160,15 +242,15 @@ public class Menu {
         System.out.println("4: Exit");
 
         var value = console.readLine();
-        switch(value) {
+        switch (value) {
             case "1":
-                getAllMedicalHistoryByOwner();
+                clinicServices.getAllMedicalHistoryByOwner();
                 break;
             case "2":
-                getAllMedicalHistoryByPet();
+                clinicServices.getAllMedicalHistoryByPet();
                 break;
             case "3":
-                getAllMedicalHistoryByVet();
+                clinicServices.getAllMedicalHistoryByVet();
                 break;
             case "4":
                 System.out.println("Exit!");
@@ -177,6 +259,75 @@ public class Menu {
                 System.out.println("Please Enter a Number");
         }
     }
-    /*Pet View, Pet list, Appointment List, Login(Owner,Vet), Regeister Owner, Add pet Data, Create Appointments(Owner, Vet)
-    * Cancel Appointments, update appointment, Create vet, Remove Vet,*/
+
+    //appointment
+
+    public void appointmentList() {
+    }
+
+    public void createAppointmentMenu() {
+        System.out.println("Enter your Pet ID: ");
+        String petId = console.readLine();
+        System.out.println("Enter Vet ID: ");
+        String vetId = console.readLine();
+        System.out.println("Enter date: ");
+        String date = console.readLine();
+        System.out.println("Enter Description: ");
+        String desc = console.readLine();
+        clinicServices.createAppointment();
+    }
+
+    public void updateAppointmentMenu(Console console) {
+        System.out.println("Enter your Pet ID: ");
+        String petId = console.readLine();
+        System.out.println("Enter Vet ID: ");
+        String vetId = console.readLine();
+        System.out.println("Enter date: ");
+        String date = console.readLine();
+        System.out.println("Enter Description: ");
+        String desc = console.readLine();
+        clinicServices.updateAppointment();
+    }
+
+    //petlist
+    public void petList() {
+    }
+
+    public void addPetData(Console console) {
+        System.out.println("Enter your Pet Name: ");
+        String name = console.readLine();
+        System.out.println("Enter your Pet Type: ");
+        String type = console.readLine();
+        System.out.println("Enter your Pet Age: ");
+        String age = console.readLine();
+        System.out.println("Enter your Pet Breed: ");
+        String breed = console.readLine();
+        System.out.println("Enter your Pet Color: ");
+        String color = console.readLine();
+        System.out.println("Enter your Pet Weight: ");
+        String weight = console.readLine();
+        System.out.println("Enter your ID: ");
+        String ownerId = console.readLine(); //same as idCard?
+        clinicServices.addPet();
+    }
+
+    //create - remove vet
+    public void createVetMenu(Console console) {
+        System.out.println("Enter your ID Card: ");
+        String idCard = console.readLine();
+        System.out.println("Enter your Password: ");
+        String password = console.readLine();
+        System.out.println("Enter your Name: ");
+        String name = console.readLine();
+        System.out.println("Enter your Address: ");
+        String address = console.readLine();
+        System.out.println("Enter your Phone Number: ");
+        String phone = console.readLine();
+        clinicServices.createVet();
+    }
+
+    /*Pet View =, Pet list, Appointment List(Cancel Appointments), Login(Owner =,Vet =), Regeister Owner =, Add pet Data, Create Appointments(Owner, Vet)
+     * update appointment, Create vet =, Remove Vet =,*/
+
+    //pet list, appointment list
 }
